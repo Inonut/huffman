650394ce3de8ba0219ced8f8ace8d2ebcabf003b
draducanu <raducanu_dragos12@yahoo.com>
2016-03-31 23:47:51 +0300

    refactorize

--- src/main/groovy/huffman/Main.groovy
+++ src/main/groovy/huffman/Main.groovy
@@ -1,16 +1,64 @@
 package huffman
 
-import huffman.util.BitInputStream
+import groovyx.gpars.group.DefaultPGroup
+
+
 
 /**
  * Created by Dragos on 29.03.2016.
  */
 class Main {
 
-    public static void main(String ...args){
+    def static group = new DefaultPGroup(200)
+    def static count = 0
 
-        def list = [[value: 1],[value: 1],[value: 1],[value: 1],[value: 1],[value: 1]]
+    def static fibAsync(int n){
+       // println n
+        if(n <=2 ){
+            return 1
+        } else {
+            if(count < 100){
+                count+=2
+                def a = group.task{ fibAsync(n-1) }
+                def b = group.task{ fibAsync(n-2) }
+                def r = a.get() + b.get()
+                count--
+                return r
+            } else {
+                return fibSync(n)
+            }
 
-        println list*.value.sum()
+
+        }
+    }
+
+    def static fibSync(int n){
+        //println n
+        if(n <=2 ){
+            return 1
+        } else {
+            return fibSync(n-2) + fibSync(n-1)
+
+        }
+    }
+
+
+    public static void main(String ...argss){
+
+        def n = 40
+
+        def t
+
+
+
+        t = System.currentTimeMillis()
+        println fibAsync(n)
+        println "2----- ${System.currentTimeMillis() - t}"
+
+        t = System.currentTimeMillis()
+        println fibSync(n)
+        println "1----- ${System.currentTimeMillis() - t}"
+
+
     }
 }
